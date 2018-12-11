becd597f5ece1d6bd633459814a48b474eaacbee
Grace Hayden <graceehayden@gmail.com>
2018-12-10 23:46:16 -0600

    refactored

--- refactored_parsing.py
+++ refactored_parsing.py
@@ -1,16 +1,14 @@
 import operator
-# import dateutil
-# from dateutil.parser import parse
-
+from datetime import datetime
 
 class Person():
 
     def __init__(self, last_name='', first_name='', gender='', fav_color='', dob=''):
 
-        self.last_name = last_name.strip().lstrip()
-        self.first_name = first_name.strip().lstrip()
+        self.last_name = last_name
+        self.first_name = first_name
 
-        gender = gender.strip().lstrip()
+        gender = gender
         if gender == 'M' or gender == 'Male':
             self.gender = 'Male'
         elif gender == 'F' or gender == 'Female':
@@ -18,25 +16,13 @@ class Person():
         else:
             self.gender = 'Unknown'
 
-        self.fav_color = fav_color.strip().lstrip()
-        self.dob = dob.strip().lstrip()
-        self.line = [self.last_name + self.first_name + self.gender +
-                     self.dob + self.fav_color]
+        self.fav_color = fav_color
 
-    def make_line(self):
-        return self.line
+        formatted_date = datetime.strptime(dob, '%m/%d/%Y')
+        self.dob = formatted_date.strftime('%m/%d/%Y')
 
-    def format_date(self):
-        # self.date.replace("-", "/")
-        try:
-            parsed_date = parse(self.date, dayfirst=True)
-        except TypeError:
-            pass
-        try:
-            parsed_date = parse(self.date)
-        except TypeError:
-            pass
-        self.date = parsed_date
+        self.line = [self.last_name, self.first_name, self.gender,
+                    self.dob, self.fav_color]
 
     def sort_by_gen_ln(self):
         '''sort by gender (females before males) then by last name ascending'''
@@ -45,7 +31,7 @@ class Person():
 
     def sort_by_dob_ln(self):
         ''' sort by birth date, ascending then by last name ascending '''
-        sorted_line = sorted(self.line, key=operator.itemgetter(4, 0))
+        sorted_line = sorted(self.line, key=operator.itemgetter(3, 0))
         return sorted_line
 
     def sort_by_ln_desc(self):
@@ -59,30 +45,23 @@ def parse_text():
     common list. Orders lines and formats data for printing.
     '''
     comma_lines = process_file("comma.txt", ',')
-    processed_comma_lines = process_comma_file(comma_lines)
+    processed_comma_lines = process_comma_file(comma_lines, ',')
 
     pipe_lines = process_file("pipe.txt", '|')
-    processed_pipe_lines = process_pipe_file(pipe_lines)
+    processed_pipe_lines = process_pipe_file(pipe_lines, '|')
 
     space_lines = process_file("space.txt", ' ')
-    processed_space_lines = process_space_file(space_lines)
+    processed_space_lines = process_space_file(space_lines, ' ')
 
     persons_list = processed_comma_lines + processed_pipe_lines + processed_space_lines
 
     output_1_sorted = []
-    for person in persons_list:
-        line = person.sort_by_gen_ln()
-        output_1_sorted.append(line)
-
     output_2_sorted = []
-    for person in persons_list:
-        line = person.sort_by_gen_ln()
-        output_2_sorted.append(line)
-
     output_3_sorted = []
-    for person in persons_list:
-        line = person.sort_by_gen_ln()
-        output_3_sorted.append(line)
+    for line in persons_list:
+        output_1_sorted.append(line.sort_by_gen_ln())
+        # output_2_sorted.append(line.sort_by_dob_ln())
+        output_3_sorted.append(line.sort_by_ln_desc())
 
     print('Output 1:')
     for line in output_1_sorted:
@@ -96,29 +75,30 @@ def parse_text():
 
 
 def process_file(file, delimiter):
+    ''' read text and split into a list of a list of strings '''
     raw_data = []
     with open(file, "r") as file:
-        for line in file:
-            line.split(delimiter)
-            raw_data.append(line.strip())
+        raw_data = [line.split(delimiter) for line in file]
     return raw_data
 
 
-def process_comma_file(raw_data):
+def process_comma_file(raw_data, delimiter):
     list = []
-    for line in raw_data:
-        line.split()
-        list.append(Person(last_name=raw_data[0],
-                       first_name=raw_data[1],
-                       gender=raw_data[2],
-                       fav_color=raw_data[3],
-                       dob=raw_data[4]))
+    for word in raw_data:
+        line = [x.strip() for x in word]
+        list.append(Person(last_name=line[0],
+                       first_name=line[1],
+                       gender=line[2],
+                       fav_color=line[3],
+                       dob=line[4]))
     return list
 
 
-def process_pipe_file(raw_data):
+def process_pipe_file(raw_data, delimiter):
     list = []
-    for line in raw_data:
+    for word in raw_data:
+        line = [x.strip() for x in word]
+        line[5] = line[5].replace("-", "/")
         list.append(Person(last_name=line[0],
                            first_name=line[1],
                            gender=line[3],
@@ -127,9 +107,11 @@ def process_pipe_file(raw_data):
     return list
 
 
-def process_space_file(raw_data):
+def process_space_file(raw_data, delimiter):
     list = []
-    for line in raw_data:
+    for word in raw_data:
+        line = [x.strip() for x in word]
+        line[4] = line[4].replace("-", "/")
         list.append(Person(last_name=line[0],
                            first_name=line[1],
                            gender=line[3],
@@ -137,6 +119,21 @@ def process_space_file(raw_data):
                            fav_color=line[5]))
     return list
 
+
+# def sort_by_gen_ln(list):
+#     '''sort by gender (females before males) then by last name ascending'''
+#     sorted_lines = sorted(list, key=operator.itemgetter(2, 0))
+#     return sorted_lines
+#
+# def sort_by_dob_ln(list):
+#     ''' sort by birth date, ascending then by last name ascending '''
+#     sorted_lines = sorted(list, key=operator.itemgetter(0))
+#     return sorted_lines
+#
+# def sort_by_ln_desc(list):
+#     ''' sort by last name, descending '''
+#     sorted_lines = sorted(list, key=operator.itemgetter(0), reverse=True)
+#     return sorted_lines
 
 if __name__ == '__main__':
     parse_text()
